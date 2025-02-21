2349daad8d4bbf7422b643be0df2e4b6844c32c4
Anton <carla.paz.api@gmail.com>
2025-02-21 14:04:23 +0100

    premières fonctions

--- othello/othello.py
+++ othello/othello.py
@@ -15,7 +15,29 @@ def autre(joueur: int) -> int:
 def peut_jouer(c: coup, joueur: int, g: grille) -> bool:
     """Cette fonction a besoin d'exister pour l'affichage.
     Vous l'écrirez en chemin """
-    return False
+    coups_dispo = []
+    for x in range(8):
+        for y in range(8):
+
+            if g[x][y] == joueur:
+
+                for dx in range(-1, 2):
+                        for dy in range(-1, 2): 
+
+                            coup_x = x + dx
+                            coup_y = y + dy
+                            encadre = False
+
+                            while 0 <= coup_x < 8 and 0 <= coup_y < 8 and g[coup_x][coup_y] == autre(joueur):
+                                coup_x += dx
+                                coup_y += dy
+                                encadre = True
+
+                            if 0 <= coup_x < 8 and 0 <= coup_y < 8 and g[coup_x][coup_y] == 0 and encadre:
+                                coups_dispo.append((coup_x, coup_y))
+    return c in coups_dispo
+                            
+
 
 def show(joueur: int, g: grille):
     """
@@ -41,3 +63,18 @@ def show(joueur: int, g: grille):
             print(" ├───"+ 7*"┼───" +"┤")
     print(f"C'est le tour du joueur {joueur}")
 
+def poser_pion(c: coup, joueur: int, g: grille):
+    g[c[0]][c[1]] = joueur
+
+def creer_grille() -> grille:
+    """
+    Fonction générant la grille de départ du jeu
+    """
+    g = [[0 for _ in range(8)] for _ in range(8)]
+
+    g[3][3], g[4][4] = 1, 1
+    g[4][3], g[3][4] = 2, 2
+
+    return g
+
+show(1, creer_grille())
